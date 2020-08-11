d7e66f33a10c810a2293d82baca56bf9de88e2b6
dhruvinparikh93 <imsilent93@gmail.com>
2020-08-11 14:24:33 -0400

    add ganache config

--- survey-marketplace-truffle/truffle-config.js
+++ survey-marketplace-truffle/truffle-config.js
@@ -48,6 +48,11 @@ module.exports = {
     //  port: 8545,            // Standard Ethereum port (default: none)
     //  network_id: "*",       // Any network (default: none)
     // },
+    ganache: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
 
     // Another network with more advanced options...
     // advanced: {
