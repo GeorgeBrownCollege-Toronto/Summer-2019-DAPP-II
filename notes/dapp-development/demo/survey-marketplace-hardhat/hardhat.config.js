9a656dae6f3f6be33ce814e86cca535f0d796d62
dhruvinparikh <imsilent93@gmail.com>
2021-04-16 16:41:40 -0400

    add ci pipeline

--- notes/dapp-development/demo/survey-marketplace-hardhat/hardhat.config.js
+++ notes/dapp-development/demo/survey-marketplace-hardhat/hardhat.config.js
@@ -1,8 +1,30 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-solhint");
+require("@nomiclabs/hardhat-etherscan");
+
+
+require("dotenv").config();
+
+const mnemonic = process.env.MNEMONIC;
+const etherscanAPI = process.env.ETHERSCAN_KEY
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_TOKEN}`,
+      accounts: { mnemonic }
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: etherscanAPI
+  }
 };
