48daa127c3d21444c49394de0c600fdcdbddbf22
Dhruvin Parikh <imsilent93@gmail.com>
2021-04-21 00:53:40 -0400

    Defi (#19)
    * vanila escrow
    
    * add aave ether escrow demo
    
    * add aave dai mainnet fork and kovan

--- notes/defi/demo/vanilla-escrow/contracts/Escrow.sol
+++ notes/defi/demo/vanilla-escrow/contracts/Escrow.sol
@@ -2,18 +2,24 @@
 pragma solidity ^0.7.5;
 
 contract Escrow {
+    // 1. Setup
     address public depositor;
     address public beneficiary;
     address public arbiter;
     
+    // 2. Constructor
+    // 3. Funding
     constructor(address _arbiter, address _beneficiary) payable {
+        // 2. Store address
         depositor = msg.sender;
         arbiter = _arbiter;
         beneficiary = _beneficiary;
     }
 
     function approve() external payable {
+        // 5. Security
         require(msg.sender == arbiter);
+        // 4. Approval
         payable(beneficiary).transfer(address(this).balance);
     }
 }
